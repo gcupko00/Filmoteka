61ca1c8560f54c4d13ed369da1b9ade6fed5d578
Antonio Zelić <azelic1606@gmail.com>
2018-01-21 23:11:01 +0100

    Added data storage and data classes

---
+++ app/src/main/java/cupkovic/fesb/hr/filmoteka/DataStorage.java
@@ -0,0 +1,40 @@
+package cupkovic.fesb.hr.filmoteka;
+
+import java.util.ArrayList;
+
+/**
+ * Created by EliteBook40 on 1/21/2018.
+ */
+
+public class DataStorage {
+    public static ArrayList<Movie> moviesList = new ArrayList<Movie>();
+    public static ArrayList<Person> personsList = new ArrayList<Person>();
+    public static ArrayList<MovieCredit> movieCreditsList = new ArrayList<MovieCredit>();
+
+    public static Movie getMovieById(Integer id) {
+        for (int i = 0; i < moviesList.size(); i++) {
+            if (moviesList.get(i).getId() == id) {
+                return moviesList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public static Person getPersonById(Integer id) {
+        for (int i = 0; i < personsList.size(); i++) {
+            if (personsList.get(i).getId() == id) {
+                return personsList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public static MovieCredit getMovieCreditById(String credit_id) {
+        for (int i = 0; i < movieCreditsList.size(); i++) {
+            if (String.valueOf(movieCreditsList.get(i).getId()) == credit_id) {
+                return movieCreditsList.get(i);
+            }
+        }
+        return null;
+    }
+}
